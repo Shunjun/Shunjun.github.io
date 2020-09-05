abfd3ffd94b42d4cfa643d826c334ea30c739881
DeadSoul <toby.zsj@gmail.com>
2020-09-06 01:57:01 +0800

    修改logo

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 const nav = require('./config/nav')
 
 module.exports = {
-  title: "Page Stranding",
+  title: "",
   description: "Just playing around",
   head: [ // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
     ['link', { rel: 'icon', href: '/img/favicon.ico' }], //favicons，资源放在public文件夹
@@ -63,7 +63,7 @@ module.exports = {
   themeConfig: {
     nav,
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
-    logo: '/img/EB-logo.png', // 导航栏logo
+    logo: '/img/PageStranding.svg', // 导航栏logo
     repo: 'Shunjun/Page-Stranding', // 导航栏右侧生成Github链接
     searchMaxSuggestions: 10, // 搜索结果显示最大数
     lastUpdated: '上次更新', // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
